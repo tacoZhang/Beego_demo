dc65952055c29e4acd0b1d6e83abb3736701eb35
ZhangCN97 <710816334@qq.com>
2019-07-24 15:14:58 +0800

    添加新配置

--- conf/Conf.go
+++ conf/Conf.go
@@ -1,5 +1,12 @@
 package conf
 
+// 时间格式化字符串
+const SysTimeform string = "2006-01-02 15:04:05"
+const SysTimeformShort string = "2006-01-02"
+
+// 中国时区
+var SysTimeLocation, _ = time.LoadLocation("Asia/Chongqing")
+
 type Conf struct {
 	MariaConf Maria
 }
